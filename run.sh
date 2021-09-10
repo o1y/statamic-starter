7642ea68fdcaa4a4c4109b868f977e61e789657b
Oliver Mesieh <olivermesieh@gmail.com>
2021-09-10 13:39:28 +0200

    Remove bindfs mount to not require insecure docker permissions anymore

--- run.sh
+++ run.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-if ! sudo mount -a 2> /dev/null; then
-	printf '\e[1;31mERROR:\e[0m %s' \
-		'Container running with improper privileges.
-
-	Be sure your service is configured with the following options:
-	___
-	services:
-	  statamic:
-		cap_add:
-		  - SYS_ADMIN
-		devices:
-		  - /dev/fuse
-		# needed on certain cloud hosts
-		security_opt:
-		  - apparmor:unconfined
-	___
-
-	OR (use first option if possible)
-	___
-	services:
-	statamic:
-		privileged: true
-	___
-
-	' | sed 's/^    //'
-	exit 1
-fi
-
 # Environment
 # ------------
 declare -x TERM="${TERM:-xterm}"
@@ -35,46 +7,28 @@ declare -x TERM="${TERM:-xterm}"
 main() {
 	init
 
-	sudo rm -f /var/run/apache2/apache2.pid
-	sudo apache2-foreground
+	php artisan serve --host 0.0.0.0 --port 8080
 }
 
 # Config Functions
 # ---------------------
 
+check_statamic() {
+	if [[ ! -f /var/www/config/app.php ]]; then
+		h2 'No Statamic found. Begin Statamic Installation ...'
+		composer create-project statamic/statamic /tmp/statamic --prefer-dist --no-progress
+		mv -n /tmp/statamic/* /var/www/
+		sudo ln -s /var/www/please /usr/local/bin/please && chown +x /usr/local/bin/please /var/www/please
+	else
+		h2 'Statamic already installed.'
+	fi
+}
+
 init() {
-	# Install Statamic
-	if [[ ! -f /app/config/app.php ]]; then
-		h1 'No Statamic found. Begin Statamic Installation ...'
-		composer create-project statamic/statamic /app --prefer-dist --no-progress
-		sudo ln -s /app/please /usr/local/bin/please && chown +x /usr/local/bin/please /app/please
-	fi
+	h1 'Prepare Statamic'
 
-	# Apache config adustments
-	sudo sed -i \
-		-e "/^[[:blank:]]*.ServerName www.example.com/{c\\" \
-		-e "\\tServerName ${SERVER_NAME:-localhost} \\" \
-		-e "\\tServerAlias www.${SERVER_NAME:-localhost}" \
-		-e '}' /etc/apache2/sites-available/000-default.conf
-
-	# Apache set Document Root
-	sudo sed -i "s@DocumentRoot.*\$@DocumentRoot ${DOCUMENT_ROOT:-/var/www/html/public}@g" \
-		/etc/apache2/sites-available/*.conf
-	
-	# Apache pass environment variables
-	sudo tee /etc/apache2/conf-enabled/environment.conf > /dev/null <<-EOF
-	SetEnv CACHE_DRIVER ${CACHE_DRIVER:-file}    
-	SetEnv REDIS_HOST ${REDIS_HOST:-redis} 
-	SetEnv REDIS_PORT ${REDIS_PORT:-6379} 
-	SetEnv MAIL_HOST ${MAIL_HOST:-mail}
-	SetEnv MAIL_PORT ${MAIL_PORT:-1025}
-	SetEnv COMPOSER_HOME ${COMPOSER_HOME:-/usr/bin/composer}
-	EOF
-
-	# Fix file permissions
-	if [[ -f /app/ ]]; then
-		sudo find /app ! \( -user admin -group admin \) -exec chown admin:admin {} \;
-	fi
+	h2 'Check installation'
+	check_statamic
 
 	h1 'Statamic is ready'
 }
